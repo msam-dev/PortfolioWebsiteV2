a33a9974ba1514d2b5d94758b2f2797e4269965b
samuel micallef <sammy.m1285@gmail.com>
2022-07-29 08:51:00 +1000

    clean up contact page code

--- src/components/ContactContent.js
+++ src/components/ContactContent.js
@@ -1,21 +1,18 @@
 import "./ContactContentStyles.css";
-import React, { useState } from 'react';
-import { GoMail } from "react-icons/go";
-import { IoLogoLinkedin } from "react-icons/io";
+import React from 'react';
 
 const ContactContent = () => {
   return (
     <div className="ContactPageContainer">
-        <div className="ContactTxt">
-            <div className="lets">Let's get in touch<span className="ContactBlue">.</span></div>
-            <div className="ContactLinks">
-                <div className="Cemail">Email me: <span className="cBlue">micallef.contact@gmail.com </span>&nbsp;</div>
-                <a href="https://www.linkedin.com/in/samuelmicallef/" className="Linked">Add me: <span className="cBlue">linkedin.com/in/samuelmicallef</span></a>
-            </div>
+      <div>
+        <div className="lets">Let's get in touch<span className="ContactBlue">.</span></div>
+        <div className="ContactLinks">
+          <div className="ContactEmail">Email me: <span className="ContactBlueUnderline">micallef.contact@gmail.com </span>&nbsp;</div>
+          <a href="https://www.linkedin.com/in/samuelmicallef/" className="Linked" target="_blank" rel="noreferrer">Add me: <span className="ContactBlueUnderline">linkedin.com/in/samuelmicallef</span></a>
         </div>
+      </div>
     </div>
   );
 };
-
 
 export default ContactContent
