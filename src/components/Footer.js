e01130d21d99a08299f48091445adac009f3a4c8
samuel micallef <sammy.m1285@gmail.com>
2022-07-29 16:21:02 +1000

    about me page and fixed scrolling on nav bar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 
 const Footer = () => {
   return (
-    <div className="header">
+    <div className="FooterContainer">
         <div className="madeWith">This website was made with React. <a href="https://github.com/Sammy-Dev/PortfolioWebsiteV2" className="view" target="_blank">View my code</a></div>
         <div className="Copyright">Copyright © 2021 Samuel Micallef. All rights reserved </div>
     </div>
