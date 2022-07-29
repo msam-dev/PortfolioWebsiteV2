b5b2cc8a37d89d4cef1f006470a9e80eeacec060
samuel micallef <sammy.m1285@gmail.com>
2022-07-29 16:22:42 +1000

    ss

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,10 @@
 import "./FooterStyles.css";
-import React, { useState } from 'react';
+import React from 'react';
 
 const Footer = () => {
   return (
     <div className="FooterContainer">
-        <div className="madeWith">This website was made with React. <a href="https://github.com/Sammy-Dev/PortfolioWebsiteV2" className="view" target="_blank">View my code</a></div>
+        <div className="madeWith">This website was made with React. <a href="https://github.com/Sammy-Dev/PortfolioWebsiteV2" className="view" target="_blank" rel="noreferrer">View my code</a></div>
         <div className="Copyright">Copyright © 2021 Samuel Micallef. All rights reserved </div>
     </div>
   );
