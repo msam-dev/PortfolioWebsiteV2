867364d9b6f0f35f4ba072b6aaa927322359debe
samuel micallef <sammy.m1285@gmail.com>
2022-08-10 16:30:53 +1000

    fix client side routing

--- src/App.js
+++ src/App.js
@@ -5,20 +5,19 @@ import About from "./routes/About";
 import Resume from "./routes/Resume";
 import Contact from "./routes/Contact";
 import Projects from "./routes/Projects";
-import { Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return( 
-      <>
-        <Routes>
-          <Route path="/PortfolioWebsiteV2" element={<Home />} />
-          <Route path="/" element={<Home />}/>
-          <Route path="/About" element={<About />}/>
-          <Route path="/Resume" element={<Resume />}/>
-          <Route path="/Contact" element={<Contact />}/>
-          <Route path="/Projects" element={<Projects />}/>
-        </Routes>
-      </>
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />}/>
+            <Route path="/About" element={<About />}/>
+            <Route path="/Resume" element={<Resume />}/>
+            <Route path="/Contact" element={<Contact />}/>
+            <Route path="/Projects" element={<Projects />}/>
+          </Routes>
+        </Router>
     );
 }
 
