f41bb69ddc114e8993075d71b299a727e5a8397d
mSam-Dev <sam.engineer0@gmail.com>
2024-06-02 11:55:30 +1000

    alingment

--- src/App.js
+++ src/App.js
@@ -9,16 +9,16 @@ import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 function App() {
   return( 
-        <Router>
-          <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/About" element={<About />}/>
-            <Route path="/Resume" element={<Resume />}/>
-            <Route path="/Contact" element={<Contact />}/>
-            <Route path="/Projects" element={<Projects />}/>
-          </Routes>
-        </Router>
-    );
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />}/>
+        <Route path="/About" element={<About />}/>
+        <Route path="/Resume" element={<Resume />}/>
+        <Route path="/Contact" element={<Contact />}/>
+        <Route path="/Projects" element={<Projects />}/>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
