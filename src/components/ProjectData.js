37a3d2791c1664c65d4b06a46a30445feca102e6
mSam-Dev <sam.engineer0@gmail.com>
2024-12-28 16:15:57 +1100

    adding quizgame to project

--- src/components/ProjectData.js
+++ src/components/ProjectData.js
@@ -2,6 +2,7 @@ import projectImage1 from "../images/projectImage1.webp"
 import projectImage2 from "../images/projectImage2.webp"
 import animateImg from "../images/animate.webp"
 import testImg from "../images/TestApp.webp"
+import defaultImg from "../images/projectImage3.webp"
 // import Blank from "../images/Blank.webp"
 
 /*
@@ -11,36 +12,44 @@ import testImg from "../images/TestApp.webp"
 
 const ProjectData = [
     {
+        imageSrc: defaultImg,
+        projectTitle: "QuizGame.top",
+        projectSummary: "QuizGame.top is a live dynamic web app which has multiple geography based quizzes. I created this site using React, TypeScript, C#, SQLite, and it is hosted using AWS.",
+        projectUrl: "https://QuizGame.top",
+        codeUrl: "https://github.com/mSam-Dev/quizgame.top",
+        detailsUrl: "https://github.com/mSam-Dev/quizgame.top/blob/main/ProjectDetails.md",
+    },
+    {
         imageSrc: projectImage1,
         projectTitle: "Portfolio Website",
-        projectSummary: "This is the portfolio website you are using right now! It was fully designed and created by me. This website was made using the React library.",
+        projectSummary: "This is the website you are using right now! msam.dev was fully designed and created by me using the React JavaScript library.",
         projectUrl: "/",
-        codeUrl: "https://github.com/Sammy-Dev/PortfolioWebsiteV2",
-        detailsUrl: "https://github.com/Sammy-Dev/PortfolioWebsiteV2/blob/master/ProjectDetails.md",
+        codeUrl: "https://github.com/mSam-Dev/PortfolioWebsiteV2",
+        detailsUrl: "https://github.com/mSam-Dev/PortfolioWebsiteV2/blob/master/ProjectDetails.md",
     },
     {
         imageSrc: testImg,
         projectTitle: "Android Test App",
         projectSummary: "A quiz app I made that lets you test your knowledge on a variety of topics. It also has user registration so you can keep track of your quiz scores. This app was made using java for android.",
         projectUrl: "https://youtu.be/hi1MHuar9vk",
-        codeUrl: "https://github.com/Sammy-Dev/TestApp",
-        detailsUrl: "https://github.com/Sammy-Dev/TestApp/blob/master/ProjectDetails.md",
+        codeUrl: "https://github.com/mSam-Dev/TestApp",
+        detailsUrl: "https://github.com/mSam-Dev/TestApp/blob/master/ProjectDetails.md",
     },
     {
         imageSrc: projectImage2,
         projectTitle: "Scientific Calculator",
         projectSummary: "A web-based calculator with some more functionality than many standard online calculators. It also includes day/night toggle. This website was made using HTML, CSS and JavaScript.",
         projectUrl: "/calculator.html",
-        codeUrl: "https://github.com/Sammy-Dev/Scientifc-Calculator",
-        detailsUrl: "https://github.com/Sammy-Dev/Scientifc-Calculator/blob/main/ProjectDetails.md",
+        codeUrl: "https://github.com/msam-dev/PortfolioWebsiteV2/tree/master/public",
+        detailsUrl: "https://github.com/msam-dev/PortfolioWebsiteV2/blob/master/public/Calculator-ProjectDetails.md",
     },
     {
         imageSrc: animateImg,
         projectTitle: "Android Animations",
         projectSummary: "A simple app I made to test and play with android’s image animation functionality. This app was made using java for android.",
         projectUrl: "https://www.youtube.com/watch?v=NXO6JAi1iu0",
-        codeUrl: "https://github.com/Sammy-Dev/AndroidAnimation",
-        detailsUrl: "https://github.com/Sammy-Dev/AndroidAnimation/blob/main/ProjectDetails.md",
+        codeUrl: "https://github.com/mSam-Dev/AndroidAnimation",
+        detailsUrl: "https://github.com/mSam-Dev/AndroidAnimation/blob/main/ProjectDetails.md",
     },
     
     /*
